871e060ae0e521f73c0256299a2e4ed68ec9a9cc
Stephen Boyle <1nobad@gmail.com>
2019-11-01 15:00:38 +0000

    map added and working

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,50 @@
   <div id="app">
     <h1>Natural Events</h1>
     <h2>The Earth Observatory Natural Event Tracker (EONET)</h2>
-    <li v-for="(title, value) in eventData">
-          <span>{{ title }}</span>
-        </li>
+    <h2>Event</h2>
+    <map-view :mapid="mapid"></map-view>
+
   </div>
 </template>
 
 <script>
 import {eventBus} from './main.js'
+import NaturalEvent from './components/NaturalEvent.vue';
+import Map from './components/Map.vue';
+
 
 export default {
   name: 'app',
+
   data() {
     return {
-      eventData: []
+      eventData: [],
+      // mapid: []
 
     }
+  },
+  components: {
+    "map-view": Map
   },
 
   mounted(){
     fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events')
     .then(response => response.json())
     .then(res => this.eventData = this.transformEvents(res.events))
+    // .then(res => this.eventData = this.transformEvents(res.events.description))
+
 
   },
   methods: {
     transformEvents(arr){
       return arr.map((event) => {
-        return [event.title, event.description, event.link, event.categories, event.sources, event.geometries]
+        return (event)
       })
     }
   },
+  filters: {
+
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { Icon } from 'leaflet'
+
 
 Vue.config.productionTip = false
 
